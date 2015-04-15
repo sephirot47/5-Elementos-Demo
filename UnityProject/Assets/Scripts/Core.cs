dfc2866b78aed3f8bbc44a0536277e54e4887e30
sephirot47 <victoriodesica777@hotmail.com>
2015-04-15 10:13:07 +0200

    Reorganized everything, camera working but in js, must change

---
+++ UnityProject/Assets/Scripts/Core.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Core : MonoBehaviour 
+{
+	public static float gravity = -10.0f;
+	void Start () {
+	
+	}
+
+	void Update () {
+	
+	}
+}
---
+++ UnityProject/Assets/Scripts/Player.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour 
+{
+	public CharacterController controller;
+
+	void Start() 
+	{
+		controller = GetComponent<CharacterController> ();
+	}
+
+	void Update () 
+	{
+		controller.Move(Vector3.up * Core.gravity * Time.deltaTime);
+		controller.Move(Vector3.forward * 0.1f);
+	}
+}
