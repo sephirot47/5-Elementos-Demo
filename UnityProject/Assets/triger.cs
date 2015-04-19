01f09a852d3d76530c8fd13303f9fffd9c6d08f4
Jeenni <jennifer_gonzalez_2@hotmail.com>
2015-04-19 02:37:11 +0200

    Trigger Created

---
+++ UnityProject/Assets/triger.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class triger : MonoBehaviour {
+
+	void OnTriggerStay (Collider col) {
+		if (col.gameObject.name == "Zap") {
+
+			Destroy(col.gameObject);
+		}
+	}
+}
