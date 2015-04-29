a308ad4a4be87da8352acd5b9d3f2afac19b4842
sephirot47 <victoriodesica777@hotmail.com>
2015-04-29 23:40:39 +0200

    Idle animation implemented

--- UnityProject/Assets/Scripts/Personajes/PlayerAnimation.cs
+++ UnityProject/Assets/Scripts/Personajes/PlayerAnimation.cs
@@ -6,34 +6,52 @@ public class PlayerAnimation : MonoBehaviour
 	Player player;
 	private Animation anim;
 
+	public float randomIdleDelay = 5.0f;
+	private float idleTime;
+
 	void Start () 
 	{
 		player = GetComponent<Player>();	
 		anim = GetComponent<Animation>();
+		idleTime = 0.0f;
 	}
 	
 	void Update() 
 	{
+		if(anim == null) return;
+
 		if(Core.paused) 
 		{
 			anim.Stop();
 			return;
 		}
 
-		if(anim)
+		if(!player.IsJumping())
 		{
-			if(!player.IsJumping())
+			Vector2 planeMovement = new Vector2(player.movement.x, player.movement.z);
+			if(planeMovement.magnitude > 0.1f)
 			{
-				Vector2 planeMovement = new Vector2(player.movement.x, player.movement.z);
-				if(planeMovement.magnitude > 0.1f)
+				Play("Run");
+			}
+			else
+			{
+				if(!anim.IsPlaying("Combo1") && !anim.IsPlaying("ReceiveDamage"))
 				{
-					Play("Run");
-				}
-				else
-				{
-					if(!anim.IsPlaying("Combo1") && !anim.IsPlaying("ReceiveDamage"))
-						anim.Stop();
-					//Play("Idle");
+					idleTime += Time.deltaTime;
+
+					if(idleTime > randomIdleDelay)
+					{
+						idleTime = 0.0f;
+						Play("Idle2");
+					}
+					else if(!anim.IsPlaying("Idle2"))
+					{
+						Play("IdleDefault");
+					}
+					else
+					{
+						idleTime = 0.0f;
+					}
 				}
 			}
 		}
